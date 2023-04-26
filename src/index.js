1a00db0a9fcbcbac5ac7e13e313f7d539e54335d
Julien Nesme <juliennesme@gmail.com>
2023-04-26 12:01:13 +0200

    ajout oswedev

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import {extname, isAbsolute} from 'path'
-import {lookFor} from './config.js'
+import { extname, isAbsolute } from 'path'
+import { lookFor } from './config.js'
 
 let indexFiles = [lookFor.map(e => `index.${e}`), ['index.json']]
 let candidates = indexFiles.map(i => i.map(f => extname(f)).concat(i.map(f => `/${f}`)))
@@ -7,21 +7,21 @@ let candidates = indexFiles.map(i => i.map(f => extname(f)).concat(i.map(f => `/
 let relSpecs = ['.', '..'], prefixes = ['./', '../', 'file://', '.\\', '..\\']
 let extToSkip = ['.js', '.cjs', '.mjs', '.json', '.node', '.wasm'], empty = [[], []]
 
-export async function resolve(specifier, {importAssertions, parentURL}, nextResolve) {
-  let isAbs = isAbsolute(specifier)
+export async function resolve(specifier, { importAssertions, parentURL }, nextResolve) {
+    let isAbs = isAbsolute(specifier)
 
-  if (!isAbs && !relSpecs.includes(specifier) && !prefixes.some(p => specifier.startsWith(p))) {
-    return await nextResolve(specifier)
-  }
+    if (!isAbs && !relSpecs.includes(specifier) && !prefixes.some(p => specifier.startsWith(p)) && !specifier.startsWith('oswedev/')) {
+        return await nextResolve(specifier)
+    }
 
-  let selfURL = new URL((isAbs ? 'file://' : '') + specifier, parentURL).href
-  let postfixes = selfURL.endsWith('/') ? indexFiles : extToSkip.includes(extname(selfURL)) ? empty : candidates
+    let selfURL = new URL((isAbs ? 'file://' : '') + specifier, parentURL).href
+    let postfixes = selfURL.endsWith('/') ? indexFiles : extToSkip.includes(extname(selfURL)) ? empty : candidates
 
-  for (let postfix of postfixes[+(importAssertions?.type === 'json')]) {
-    try {
-      return await nextResolve(selfURL + postfix)
-    } catch {}
-  }
+    for (let postfix of postfixes[+(importAssertions?.type === 'json')]) {
+        try {
+            return await nextResolve(selfURL + postfix)
+        } catch { }
+    }
 
-  return await nextResolve(selfURL)
+    return await nextResolve(selfURL)
 }
